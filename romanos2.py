56fec35e0062dfaf7872a6b86cb4eeda0d45b473
Víctor R <vrg1984@gmail.com>
2022-07-28 10:24:46 +0200

    Versión con la función entero_a_romano

--- romanos2.py
+++ romanos2.py
@@ -1,3 +1,28 @@
+componentes = {
+    1000: 'M', 2000: 'MM', 3000: 'MMM', 
+    100: 'C', 200: 'CC', 300: 'CCC',
+    400: 'CD', 500: 'D', 600: 'DC',
+    700: 'DCC', 800: 'DCCC', 900: 'CM',
+    10: 'X', 20: 'XX', 30: 'XXX',
+    40: 'XL', 50: 'L', 60: 'LX',
+    70: 'LXX', 80: 'LXXX', 90: 'XC',
+    1: 'I', 2: 'II', 3: 'III',
+    4: 'IV', 5: 'V', 6: 'VI',
+    7: 'VII', 8: 'VIII', 9: 'IX'}
+
 class RomanNumberError(Exception):
     pass
+
+def entero_a_romano(valor):
+    valor = str(valor)
+    romano = ""
+    ix = 0
+    
+    for caracter in valor:    
+        caracter = caracter + ("0" * int(len(valor) - ix - 1))
+        romano += componentes[int(caracter)]
+        ix += 1
+    
+    return romano
+
 
--- test_romanos2.py
+++ test_romanos2.py
@@ -3,5 +3,8 @@ from romanos2 import entero_a_romano
 def test_3_es_igual_a_2_mas_1():
     assert 2 + 1 == 3
 
-def test_error_si_emtero_mayor_de_3999():
-    assert entero_a_entero(1994) == 'MCMXCIV'
+def test_entero_a_romano_1994():
+    assert entero_a_romano(1994) == 'MCMXCIV'
+
+def test_entero_a_romano_21():
+    assert entero_a_romano(21) == 'XXI'
